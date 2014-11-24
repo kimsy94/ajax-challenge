46e88f0fe6f2959610bc64ff0a8f7b456b1b7c70
Sunyoon Kim <kimsunyoon@gmail.com>
2014-11-23 19:51:56 -0800

    11/23/2014

--- js/app.js
+++ js/app.js
@@ -3,3 +3,12 @@
     app.js, main Angular application script
     define your module and controllers here
 */
+
+angular.module('CommentApp', ['ui.bootstrap'])
+    .config(function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = '2MBT8XUgqeZAXTjhXWFKDsqTFLHNb1pwyffe6iXy';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'pe9QIaMBAKtubRBUOeUZCNU1xciiNdLcygWmxIEF';
+    })
+    .controller('CommentsController', function($scope, $http) {
+
+    })
